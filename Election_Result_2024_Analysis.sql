5951fc267d4e92f13e9026e62b2b8788ce41fb31
Pushpak Agarwal <102311296+pushpakagarwal9@users.noreply.github.com>
2025-07-24 18:08:24 +0530

    Update Election_Result_2024_Analysis.sql

--- Election_Result_2024_Analysis.sql
+++ Election_Result_2024_Analysis.sql
@@ -1,1 +1,13 @@
+/* 
+===================================================================
+ Project:        Indian Lok Sabha Election 2024 - SQL Analysis
+ Author:         Pushpak Agarwal
+ Description:    This script analyzes the 2024 lok sabha election 
+                 dataset using various SQL techniques including 
+                 basic, intermediate, and advanced SQL queries.
+
+===================================================================
+*/
+
+
 
